0ad8e238cf9c607b49dd5fa253f24c43eb5900a2
roblesalfredo <37293733+roblesalfredo@users.noreply.github.com>
2021-02-03 23:48:36 +0100

    Exercises update

--- basics/funciones_frecuencia_palabras.py
+++ basics/funciones_frecuencia_palabras.py
@@ -4,6 +4,19 @@ Escribir un programa que reciba una cadena de caracteres y devuelva un diccionar
 text = 'Como quieres que te quiera si el que quiero que me quiera no me quiere como quiero que me quiera'
 """
 
+from collections import Counter
+
+
+def frecuencia_palabras_counter(texto):
+    c = Counter(texto.split())
+    return dict(c)
+
+
+def palabra_mas_repetida_counter(diccionario_palabras):
+    c = Counter(diccionario_palabras)
+    return c.most_common(1)[0]
+
+
 def frecuencia_palabras(texto):
     dic_frec = {}
     print(texto.split())
@@ -18,4 +31,22 @@ def frecuencia_palabras(texto):
         print(dic_frec)
     return dic_frec
 
-frecuencia_palabras("Como quieres quieres")
+
+def palabra_mas_repetida(diccionario_palabras):
+    frecuencia_max = 0
+    for t in diccionario_palabras.items():
+        if t[1] > frecuencia_max:
+            tupla_max = t
+            frecuencia_max = t[1]
+    return tupla_max
+
+
+# Main
+mi_diccionario = frecuencia_palabras("Como quieres quieres mesa quieres")
+tupla_max = palabra_mas_repetida(mi_diccionario)
+print(f"La palabra más repetida es '{tupla_max[0]}' con frecuencia {tupla_max[1]}.")
+
+print("\nUsando Counter")
+mi_diccionario = frecuencia_palabras_counter("Como quieres quieres mesa quieres")
+tupla_max = palabra_mas_repetida_counter(mi_diccionario)
+print(f"La palabra más repetida es '{tupla_max[0]}' con frecuencia {tupla_max[1]}.")
