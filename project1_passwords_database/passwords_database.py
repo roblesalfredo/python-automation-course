7ff2c1690f13ab152eb9018683dab0db2c294dbf
roblesalfredo <37293733+roblesalfredo@users.noreply.github.com>
2021-02-06 12:44:16 +0100

    Project 1 update

--- project1_passwords_database/passwords_database.py
+++ project1_passwords_database/passwords_database.py
@@ -4,9 +4,9 @@ Como administrador de una base de datos de un sitio web e-commerce deberás cons
     1. Generar contraseña aleatoria. Al seleccionar esta opción, se le preguntará al usuario en primer lugar, de cuántos caracteres quiere generar la contraseña. A continuación, se le preguntará al usuario cuántos números como mínimo deberá contener la contraseña e igualmente se realizará para los caracteres especiales. Se deberá mostrar por pantalla la contraseña generada y preguntar al usuario si quiere generar otra o no. Por último, el usuario será preguntado si se desea copiar la contraseña al portapapeles.
     Puede ser de ayuda conocer las funciones chr() y ord().
     
-    2. Login en sitio web. El usuario deberá introducir sus credenciales de acceso para el sitio web. El programa deberá comparar si el hash de la contraseña introducida corresponde con el valor hash almacenado en la base de datos.
+    2. Login en sitio web. El usuario deberá introducir sus credenciales de acceso para el sitio web. El programa deberá comparar si el hash de la contraseña introducida corresponde con el valor hash almacenado en la base de datos. El programa devolverá si el login ha sido correcto o si los credenciales no son los correctos.
     
-    3. Cambio de contraseña. El usuario deberá introducir la contraseña actual y, a continuación, ser preguntado por la nueva contraseña. Finalmente, el hash de la nueva contraseña será actualizado en la base de datos.
-"""
+    3. Cambio de contraseña. El usuario deberá introducir la contraseña actual y, a continuación, ser preguntado por la nueva contraseña. Finalmente, el hash de la nueva contraseña será actualizado en la base de datos y se deberá informar al usuario.
 
-print()
+El usuario será continuamente preguntado para introducir una de las opciones anteriores hasta que decida salir del programa.
+"""
