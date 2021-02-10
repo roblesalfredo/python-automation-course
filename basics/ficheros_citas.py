bd63d635d2918c2069f7702dbc25620a09df6be6
roblesalfredo <37293733+roblesalfredo@users.noreply.github.com>
2021-02-10 20:38:50 +0100

    Exercises update

--- basics/ficheros_citas.py
+++ basics/ficheros_citas.py
@@ -5,3 +5,60 @@ Escribir un programa para gestionar las citas de una consulta médica. La base d
     - Una función que reciba un dni y devuelva una lista con las citas de ese paciente.
     - Una función para eliminar las citas anteriores a una fecha dada.
 """
+
+import os
+
+
+DATABASE_FILEPATH = "citas.csv"
+
+
+def añadir_cita(dni, mes, dia, hora, especialidad, database_filepath=DATABASE_FILEPATH):
+    """Añadir cita a la agenda
+
+    Parámetros:
+        dni: string
+        mes: string con el número del mes
+        dia: string con el día en formato numérico
+        hora: string con formato hh:mm
+        especialidad: string
+    """
+    # Comprobar que la base de datos existe
+    if not os.path.isfile(database_filepath):
+        f = open(database_filepath, 'x')
+        f.close()
+
+    with open(database_filepath, 'a', encoding='utf-8') as f:
+        cita = dni + "," + mes + "," + dia + "," + hora + "," + especialidad + "\n"
+        f.write(cita)
+
+
+def listar_citas(dni, database_filepath=DATABASE_FILEPATH):
+    with open(database_filepath, 'r', encoding='utf-8') as f:
+        citas = f.readlines()
+        listado = []
+        for cita in citas:
+            dni_cita = cita.split(",")[0]
+            if dni_cita == dni:
+                listado.append(cita)
+    return listado
+
+
+def eliminar_citas(mes, dia, database_filepath=DATABASE_FILEPATH):
+    with open(database_filepath, 'r', encoding='utf-8') as f:
+        citas = f.readlines()
+
+    listado = []
+    for cita in citas:
+        mes_dia_cita = cita.split(",")[1] + cita.split(",")[2]
+        if mes_dia_cita > mes + dia:
+            listado.append(cita)
+
+    with open(database_filepath, 'w', encoding='utf-8') as f:
+        f.write("".join(listado))
+
+
+# Main
+# añadir_cita("01234567", "3", "20", "15:00", "Traumatólogo")
+listado_citas = listar_citas("345345354")
+print(listado_citas)
+eliminar_citas("3", "2")
