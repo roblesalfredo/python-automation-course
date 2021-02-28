77d80138bc6f6823450adcd001fe1cea9f1e8d55
roblesalfredo <37293733+roblesalfredo@users.noreply.github.com>
2021-02-28 13:34:06 +0100

    Added Project 2

---
+++ project_2_practicing_bs4/bs4_project.py
@@ -0,0 +1,7 @@
+"""
+Esta práctica consistirá en escoger una página web de vuestra elección, que sea susceptible de ser escrapeada utilizando las librerías requests y Beautiful Soup.
+
+Los únicos requisitos son:
+    - Obtener cualquier tipo de información de forma recursiva y ordenada en formato tabular (direcciones, resultados, valores numéricos)
+    - Almacenar los datos escrapeados en un fichero Excel utilizando el método to_excel de la librería pandas
+"""
